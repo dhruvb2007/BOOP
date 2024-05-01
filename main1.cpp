6aff91aeb4e8f0f9f1997d5fcad60fa4eeee81a5
Dhurv Prajapati <159237463+dhruvb2007@users.noreply.github.com>
2024-05-01 09:12:04 +0530

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int n1, n2, sum, sub, mul, div;
+	
+	cout<<"Enter the Number 1"<<endl;
+	cin>>n1;
+	
+	cout<<"Enter the Number 2"<<endl;
+	cin>>n2;
+	
+	cout<<"Addition is = "<<n1+n2<<endl;
+	cout<<"Substraction is = "<<n1-n2<<endl;
+	cout<<"multiplication is = "<<n1*n2<<endl;
+	cout<<"Divition is = "<<n1/n2<<endl;
+
+	return 0;
+}
